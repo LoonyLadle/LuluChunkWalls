0d65aecc155706b992a0e86c90c4dbc5ede33ee2
Lulu <48167745+LoonyLadle@users.noreply.github.com>
2019-08-05 14:39:31 -0400

    2.0.0
    Removed the chunkwall intermediate object, allowing natural stone walls to be built directly (which also enables the use of the "Build Copy" option in the game).
    
    Any unbuilt or partially built chunkwalls in an existing save will disappear and generate harmless errors on load. Thing can safely be ignored (I hope).

---
+++ Source/LuluChunkWalls/MyDesignatorDropdownGroupDefOf.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.ChunkWalls
+{
+    [DefOf]
+    public static class MyDesignatorDropdownGroupDefOf
+    {
+        static MyDesignatorDropdownGroupDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(DesignatorDropdownGroupDef));
+        }
+
+        public static DesignatorDropdownGroupDef LuluChunkWalls_NaturalWall;
+    }
+}
---
+++ Source/LuluChunkWalls/Startup.cs
@@ -0,0 +1,83 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.ChunkWalls
+{
+    [StaticConstructorOnStartup]
+    public static class Startup
+    {
+        static Startup()
+        {
+            List<ThingDef> impliedDefs = new List<ThingDef>();
+            StringBuilder stringBuilder = new StringBuilder();
+            bool first = true;
+
+            stringBuilder.Append("[LuluChunkWalls] Dynamic patched the following defs: ");
+
+            foreach (ThingDef thingDef in DefDatabase<ThingDef>.AllDefs)
+            {
+                if (thingDef.building?.isNaturalRock ?? false)
+                {
+                    ThingDef chunkDef = thingDef.building.mineableThing;
+
+                    if (chunkDef?.thingCategories?.Contains(ThingCategoryDefOf.StoneChunks) ?? false)
+                    {
+                        // Ensure lists are initialized.
+                        if (thingDef.building.blueprintGraphicData == null) thingDef.building.blueprintGraphicData = new GraphicData();
+                        if (thingDef.costList == null)                      thingDef.costList                      = new List<ThingDefCountClass>();
+                        if (thingDef.researchPrerequisites == null)         thingDef.researchPrerequisites         = new List<ResearchProjectDef>();
+                        if (thingDef.statBases == null)                     thingDef.statBases                     = new List<StatModifier>();
+
+                        // Patch the def.
+                        thingDef.building.blueprintGraphicData.texPath = "Lulu/ChunkWalls/ChunkWalls_Blueprint_Atlas";
+                        thingDef.constructionSkillPrerequisite = 6;
+                        thingDef.costList.Add(new ThingDefCountClass(chunkDef, 1));
+                        thingDef.designationCategory = DesignationCategoryDefOf.Structure;
+                        thingDef.designatorDropdown = MyDesignatorDropdownGroupDefOf.LuluChunkWalls_NaturalWall;
+                        thingDef.placingDraggableDimensions = 1;
+                        thingDef.researchPrerequisites.Add(MyResearchProjectDefOf.Stonecutting);
+                        thingDef.statBases.Add(new StatModifier { stat = StatDefOf.WorkToBuild, value = 2430 });
+                        thingDef.terrainAffordanceNeeded = TerrainAffordanceDefOf.Heavy;
+                        thingDef.uiIconPath = "Lulu/ChunkWalls/ChunkWalls_MenuIcon";
+
+                        // Manually create implied defs (we're past the point where they'd be generated automatically).
+                        impliedDefs.Add((ThingDef)typeof(ThingDefGenerator_Buildings).GetMethod("NewBlueprintDef_Thing", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[] { thingDef, false, null }));
+                        impliedDefs.Add((ThingDef)typeof(ThingDefGenerator_Buildings).GetMethod("NewFrameDef_Thing",     BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[] { thingDef }));
+
+                        // Finalize graphic data.
+                        thingDef.ResolveReferences();
+                        thingDef.PostLoad();
+
+                        // Build the log string.
+                        if (first)
+                        {
+                            stringBuilder.Append(thingDef.defName);
+                            first = false;
+                        }
+                        else
+                        {
+                            stringBuilder.AppendWithComma(thingDef.defName);
+                        }
+                    }
+                }
+            }
+
+            // Add the created implied defs to the def database.
+            foreach (ThingDef impliedDef in impliedDefs)
+            {
+                DefDatabase<ThingDef>.Add(impliedDef);
+                //impliedDef.ResolveReferences();
+            }
+            
+            DesignationCategoryDefOf.Structure.ResolveReferences();
+            Log.Message(stringBuilder.ToString());
+        }
+    }
+}
